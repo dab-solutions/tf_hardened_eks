0ddef9d1613eab4e8fd60ec26d504b525c9d22ff
davinerd <anathema@anche.no>
2022-01-16 13:14:53 +0100

    Use module v17.24.0

--- main.tf
+++ main.tf
@@ -6,31 +6,40 @@ provider "kubernetes" {
 
 module "eks" {
   source          = "terraform-aws-modules/eks/aws"
-  version         = "18.0.1"
+  #version         = "18.0.1"
+  version = " 17.24.0"
 
   cluster_version = var.cluster_version
   cluster_name    = var.cluster_name
   vpc_id          = var.create_vpc ? module.vpc[0].vpc_id : var.vpc_id
-  subnet_ids      = var.create_vpc ? module.vpc[0].private_subnets : var.vpc_subnets
+  #subnet_ids      = var.create_vpc ? module.vpc[0].private_subnets : var.vpc_subnets
+  subnets      = var.create_vpc ? module.vpc[0].private_subnets : var.vpc_subnets
+
 
   cluster_enabled_log_types = var.enable_cluster_logs ? var.cluster_enabled_log_types : []
 
-  self_managed_node_group_defaults = var.self_managed_nodes ? {
+  #self_managed_node_group_defaults = var.self_managed_nodes ? {
+  #  ami_type  = var.ami_type
+  #  disk_size = var.disk_size
+  #} : {}
+
+  #eks_managed_node_group_defaults = var.eks_managed_nodes ? {
+  #  ami_type  = var.ami_type
+  #  disk_size = var.disk_size
+  #} : {}
+
+  node_groups_defaults = {
     ami_type  = var.ami_type
     disk_size = var.disk_size
-  } : {}
+  }
+  node_groups = var.node_groups
 
-  eks_managed_node_group_defaults = var.eks_managed_nodes ? {
-    ami_type  = var.ami_type
-    disk_size = var.disk_size
-  } : {}
+  #self_managed_node_groups = var.self_managed_nodes ? var.node_groups : {}
 
-  self_managed_node_groups = var.self_managed_nodes ? var.node_groups : {}
+  #eks_managed_node_groups = var.eks_managed_nodes ? var.node_groups : {}
 
-  eks_managed_node_groups = var.eks_managed_nodes ? var.node_groups : {}
-
-  #map_roles = var.map_roles
-  #map_users = var.map_users
+  map_roles = var.map_roles
+  map_users = var.map_users
 
   tags = var.cluster_tags
 }
--- output.tf
+++ output.tf
@@ -3,10 +3,10 @@ output "cluster_endpoint" {
   value       = module.eks.cluster_endpoint
 }
 
-output "config_map_aws_auth" {
-  description = "A kubernetes configuration to authenticate to this EKS cluster."
-  value       = module.eks.aws_auth_configmap_yaml
-}
+#output "config_map_aws_auth" {
+#  description = "A kubernetes configuration to authenticate to this EKS cluster."
+#  value       = module.eks.aws_auth_configmap_yaml
+#}
 
 output "kubeconfig" {
     value = "aws eks --region ${data.aws_region.current.name} update-kubeconfig --name ${var.cluster_name}"
